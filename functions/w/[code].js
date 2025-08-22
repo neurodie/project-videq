eb428c54cc107a4b77bd2a7df83c1eb491421b5c
neurodie <krulchifer01@gmail.com>
2025-08-23 00:07:53 +0800

    main update

--- functions/w/[code].js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-  const { params, request, env } = context;
-  const url = new URL(request.url);
-  url.pathname = "/watch";
-  url.search = `?v=${encodeURIComponent(params.code)}`;
-  return env.ASSETS.fetch(new Request(url.toString(), request));
-}
